d488f9dc249c286fd3e1eb4669335d14e9ba76cf
yexinrong <yexinrong18@163.com>
2022-07-19 18:34:40 +0800

    1.添加错误常量。

--- ActorFramework/Error.cs
+++ ActorFramework/Error.cs
@@ -12,6 +12,26 @@ namespace ActorFramework
         public string Message { get; set; } = "";
         public int Code { get; set; }
 
+        public Error() { }
+
+        public Error(int code, string message)
+        {
+            IsError = true;
+            Code = code;
+            Message = message;
+        }
+
+        /// <summary>
+        /// 创建try catch到的错误
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static Error CatchError(string message)
+        {
+            return new Error(-2, message);
+        }
+
         public static Error NoError = new Error() { IsError = false };
+        public static Error InvalidMessage = new Error(-1, "该消息不属于本Actor可执行的消息");
     }
 }
